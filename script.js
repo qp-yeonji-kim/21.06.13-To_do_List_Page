351ec6a9d76192de5a2df6996adbf97df78fa496
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-06-13 19:20:34 +0900

    greetUser-오류수정
    새로고침 시 새로운 이름을 저장하지 못하고 저장되지 않는 오류 수정

--- script.js
+++ script.js
@@ -23,29 +23,30 @@ function getTime(){
   min = today.getMinutes(),
   sec = today.getSeconds();
 
-timeContainer.innerHTML = `<span>${year}년 ${month < 10 ? `0${month}` : month}월 ${date < 10 ? `0${date}` : date}일 ${days[day]}요일</span> <span>${hour < 12 ? `오전 ${hour}` : `오후 ${hour - 12 }`}시 ${min}분 ${sec}초</span>`;
+timeContainer.innerHTML = `<span>${year}년 ${month < 10 ? `0${month}` : month}월 ${date < 10 ? `0${date}` : date}일 ${days[day]}요일</span> <span>${hour < 12 ? `오전` : `오후`} ${hour < 13 ? hour : hour - 12}시 ${min}분 ${sec}초</span>`;
 }
 
 function saveName(username){
-  localStorage.setItem('userName',username);
+  localStorage.setItem('userName', username);
 }
 
 function sayHello(username){
   questionName.classList.add('hide');
   helloUser.classList.remove('hide');
   helloUser.innerText = `${username}님 안녕하세요?`;
-  answerName.value = "";
-  answerName.placeholder = '훙 제 이름 이거 아니에요!';
+  answerName.placeholder = '저 다른 이름 쓸게요';
+  askName();
 }
-//오류! 창을 껐다가 다시 키면 saveName이 되지 않는다.
 
 function askName(){
-  //nameContainer.classList.add('show'); default가 떠있는걸로 하면 되는 거 아닌가?
   nameContainer.addEventListener('submit', function(event){
     event.preventDefault();
     let userName = answerName.value;
-    sayHello(userName);
-    saveName(userName);
+    if(userName){
+      sayHello(userName);
+      saveName(userName);
+    }
+    answerName.value = "";
   });
 }
 
